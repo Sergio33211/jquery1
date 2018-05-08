0d9cf122690e361c8e9720eeb851ad6567ff1555
bohemm <bohemmm@gmail.com>
2018-05-08 12:00:13 +0200

    primer commit

---
+++ js/mein.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    //crea 2 variables i comparalas. si son iguales mostras
+    //"son iguales";en caso contrario pantalla "no son iguales"
+    //var num1 = 1;
+    //var num2 = 3;
+    //if ( num1==num2 ){
+    //console.log("son iguales");
+    //}else{
+        //console.log("no son iguales");}
+    var coches = ["ford","renault","ferari","nisan","toyota"];
+    for(i=0;i<coches.length;i++)
+        console.log(coches[i]);
+});
