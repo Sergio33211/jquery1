480d80a547adb1e9778a337a7ac9a0facd856704
bohemm <bohemmm@gmail.com>
2018-05-09 10:09:00 +0200

    Repositorio Sergio

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    //crea 2 variables y comparalas. si son iguales mostralas
+    //"son iguales";en caso contrario pantalla "no son iguales"
+    //var num1 = 1;
+    //var num2 = 3;
+    //if ( num1==num2 ){
+    //console.log("son iguales");
+    //}else{
+        //console.log("no son iguales");}
+    var coches = ["ford","renault","ferrari","nissan","toyota"];
+    for(i=0;i<coches.length;i++)
+        console.log(coches[i]);
+});
